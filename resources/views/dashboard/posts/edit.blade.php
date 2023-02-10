8995f8328bbcf2d9e2ea5dda6d606349dd43981f
Moh Agus Fathur Rozi <gusfathur25@gmail.com>
2023-02-10 22:00:52 +0700

    ep.23 authorization

--- resources/views/dashboard/posts/edit.blade.php
+++ resources/views/dashboard/posts/edit.blade.php
@@ -49,6 +49,7 @@
             <div class="mb-3">
                 <label for="image" class="form-label @error('image') is-invalid @enderror">Post Image</label>
                 <input type="hidden" name="oldImage" value="{{ $post->image }}">
+
                 @if ($post->image)
                     <img src="{{ asset('storage/' . $post->image) }}" class="img-preview img-fluid mb-3 col-sm-5 d-block"
                         alt="">
@@ -56,6 +57,7 @@
                     <img class="img-preview img-fluid mb-3 col-sm-5" alt="">
                 @endif
                 <input class="form-control" type="file" id="image" name="image" onchange="previewImage()">
+
                 @error('image')
                     <div class="invalid-feedback">
                         {{ $message }}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AdminCategoryController;
+use App\Http\Controllers\CokController;
 use App\Http\Controllers\DashboardPostController;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
@@ -89,3 +91,5 @@ Route::get('/dashboard/posts/checkSlug', [DashboardPostController::class, 'check
     ->middleware('auth');
 
 Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
+
+Route::resource('/dashboard/categories', AdminCategoryController::class)->except('show');
